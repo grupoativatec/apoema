cee34ad492d6d8e88f5300de5d7d235bd01c34bc
MichaelDu4rte <michaelirvineduarte@gmail.com>
2025-06-17 18:13:28 -0300

    change http for https

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://172.30.20.246:3009', {
+const socket = io('https://172.30.20.246:3009', {
   path: '/realtime', // 👈 importante combinar com o backend
   transports: ['websocket'], // força WebSocket (evita polling)
 });
