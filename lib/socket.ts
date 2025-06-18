c3a066ea70d8051c8b50b32622d307ecd346791a
MichaelDu4rte <michaelirvineduarte@gmail.com>
2025-06-18 07:43:08 -0300

    fix: point Socket.IO connection to production domain with HTTPS

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,8 +1,9 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://172.30.20.246:3009', {
-  path: '/realtime', // 👈 importante combinar com o backend
-  transports: ['websocket'], // força WebSocket (evita polling)
+const socket = io('https://apoema.grupoativa.net', {
+  path: '/realtime',
+  transports: ['websocket'],
 });
+
 
 export default socket;
