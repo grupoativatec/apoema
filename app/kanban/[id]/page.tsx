3fd03b1f7dd228243f3fad97c961c6b1ebffcdd6
MichaelDu4rte <michaelirvineduarte@gmail.com>
2025-06-18 07:19:01 -0300

    fix: disable SSR for KanbanBoard to prevent document is not defined error

--- app/kanban/[id]/page.tsx
+++ app/kanban/[id]/page.tsx
@@ -1,15 +1,19 @@
-import KanbanBoard from '@/components/KanbanBoard';
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const KanbanBoard = dynamic(() => import('@/components/KanbanBoard'), {
+  ssr: false,
+});
 
 interface Props {
   params: { id: string };
 }
 
 export default function KanbanPage({ params }: Props) {
-  const { id } = params;
-
   return (
     <div className="w-full h-full">
-      <KanbanBoard kanbanId={id} />
+      <KanbanBoard kanbanId={params.id} />
     </div>
   );
 }
